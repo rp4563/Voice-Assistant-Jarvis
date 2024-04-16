5131a9b7fc2bbc6adad6acf7fbe93b4465908d57
rp4563 <rp4563@srmist.edu.in>
2024-04-16 21:29:00 +0530

    Jarvis

---
+++ Jarvis_main.py
@@ -0,0 +1,98 @@
+import pyttsx3
+import datetime
+import speech_recognition
+import requests
+from bs4 import BeautifulSoup
+engine=pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice",voices[0].id)
+engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r=speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening......")
+        r.pause_threshold=1
+        r.energy_threshold=300
+        audio=r.listen(source,0,4)
+
+    try:
+        print("Understanding..")
+        query=r.recognize_google(audio,language='en-in')    
+        print(f"You Said: {query}")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+
+
+
+
+if __name__ == "__main__":
+    while True:
+        query=takeCommand().lower()
+        if "wake up" in query:
+            from GreetMe import greetMe
+            greetMe()
+
+            while True:
+                query=takeCommand().lower()
+                if "go to sleep" in query:
+                    speak("Ok sir,You can call me anytime")
+                    break
+
+                elif "hello" in query:
+                    speak("Hello sir,How are you?")
+                elif "i am fine" in query:
+                    speak("that's great  sir")
+                elif "how are you" in query:
+                    speak("perfect sir")
+                elif "thank u" in query:
+                    speak("you are welcome, sir")
+
+                elif "open" in query:
+                    from Dictapp import openappweb
+                    openappweb(query)
+                elif "close" in query:
+                    from Dictapp import closeappweb
+                    closeappweb(query)  
+
+                elif "google" in query:
+                    from SearchNow import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                    from SearchNow import searchYoutube
+                    searchYoutube(query)
+                elif "wikipedia" in query:
+                    from SearchNow import searchWikipedia
+                    searchWikipedia(query)  
+
+                elif "temperature" in query:
+                    search="temperature in chennai"
+                    url=f"https://www.google.com/search?q={search}"
+                    r=requests.get(url)
+                    data=BeautifulSoup(r.text,"html.parser")
+                    temp=data.find("div", class_="BNeawe").text
+                    speak(f"current{search} is {temp}")
+
+                elif "weather" in query:
+                    search="temperature in chennai"
+                    url=f"https://www.google.com/search?q={search}"
+                    r=requests.get(url)
+                    data=BeautifulSoup(r.text,"html.parser")
+                    temp=data.find("div", class_="BNeawe").text
+                    speak(f"current{search} is {temp}") 
+
+                elif "the time" in query:
+                    strTime=datetime.datetime.now().strftime("%H:%M")
+                    speak(f"Sir, the time is {strTime}")  
+
+                elif "finally sleep" in query:
+                    speak("Going to sleep,sir")
+                    exit()         
+
+ 
